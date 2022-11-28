81155d007e36b6d3a9bdb698aff749bda6af4320
Notarin <424c414e4b@gmail.com>
2022-11-28 01:53:05 -0500

    Check if db exists, if not, create

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import SQL.SQLHandler;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         SQLHandler.init();
     }
 }
--- src/main/java/SQL/SQLHandler.java
+++ src/main/java/SQL/SQLHandler.java
@@ -1,7 +1,34 @@
 package SQL;
 
-public class SQLHandler {
-    public static void init() {
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class SQLHandler {
+    public static void init() throws ClassNotFoundException {
+        checks();
+    }
+    public static boolean checks() throws ClassNotFoundException {
+        File db = new File("database.db");
+        if (!db.exists() || db.isDirectory()) {
+            createDB();
+        }
+        return true;
+    }
+    public static void createDB() throws ClassNotFoundException {
+        String url = "jdbc:sqlite:database.db";
+        System.out.println(url);
+        Class.forName("org.sqlite.JDBC");
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:database.db")) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
