8703078a052ca66c8d806cff3465959b4846c595
Notarin <424c414e4b@gmail.com>
2022-11-28 06:01:36 -0500

    added createUsersStructure and initDB

--- src/main/java/SQL/SQLHandler.java
+++ src/main/java/SQL/SQLHandler.java
@@ -1,19 +1,21 @@
 package SQL;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLHandler {
     public static void init() throws ClassNotFoundException, SQLException {
         checks();
         Connection connection = connect();
+        connection.close();
     }
     public static void checks() throws ClassNotFoundException, SQLException {
         File db = new File("database.db");
         if (!db.exists() || db.isDirectory()) {
             createDB();
+            Connection connection = connect();
+            initDB(connection);
+            connection.close();
         }
     }
     public static void createDB() throws ClassNotFoundException, SQLException {
@@ -26,5 +28,16 @@ public class SQLHandler {
         Class.forName("org.sqlite.JDBC");
         Connection conn = DriverManager.getConnection("jdbc:sqlite:database.db");
         return conn;
+    }
+    public static void createUsersStructure(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute("CREATE TABLE IF NOT EXISTS users (\n" +
+                "id integer PRIMARY KEY,\n" +
+                "username text NOT NULL,\n" +
+                "password text NOT NULL,\n" +
+                "discordId text)");
+    }
+    public static void initDB(Connection connection) throws SQLException {
+        createUsersStructure(connection);
     }
 }
