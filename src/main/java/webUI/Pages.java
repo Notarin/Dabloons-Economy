827a2847f18420a85cb9ece5a227f461cbbdf67f
Notarin <424c414e4b@gmail.com>
2022-12-08 03:17:09 -0500

    Add indexHtml method to PageData class and use it in Pages.load()

--- src/main/java/webUI/Pages.java
+++ src/main/java/webUI/Pages.java
@@ -2,11 +2,26 @@ package webUI;
 
 import io.javalin.Javalin;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Pages {
     public static void load(Javalin server) {
-        server.get("/", ctx -> ctx.result("Hello World"));
+        server.get("/", ctx -> ctx.html(PageData.indexHtml()));
         server.get("/debug", ctx -> ctx.result(
                 "ctx.ip() : " + ctx.ip() + "\n"
         ));
+    }
+}
+
+class PageData {
+    public static String indexHtml() throws IOException {
+        String indexHtml;
+
+        Path path = Paths.get("Pages/index.html");
+        indexHtml = new String(Files.readAllBytes(path));
+        return indexHtml;
     }
 }
