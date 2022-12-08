458cada8c30742f76882f039c843d698d755c7f1
Notarin <424c414e4b@gmail.com>
2022-12-08 02:07:02 -0500

    Simplify Javalin.create() call in WebServer
    This commit simplifies the `Javalin.create()` call in the WebServer class by removing the unused `config` parameter. This makes the code cleaner and easier to read.

--- src/main/java/webUI/WebServer.java
+++ src/main/java/webUI/WebServer.java
@@ -6,7 +6,7 @@ import io.javalin.Javalin;
 public class WebServer {
     public static void init(Config config) {
         //See https://javalin.io/documentation
-        Javalin server = Javalin.create(/*config*/);
+        Javalin server = Javalin.create();
         Pages.load(server);
         server.start(config.httpServerPort());
     }
