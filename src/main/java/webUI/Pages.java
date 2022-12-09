c559a112943ccd175eec8f73c65b3607b4fdf7ea
Notarin <424c414e4b@gmail.com>
2022-12-09 00:21:39 -0500

    Add comments and TODO to Pages.java
    This change adds comments to the Pages.java file to provide more information about what the code is doing and why. It also adds a TODO comment to indicate that there is a planned change to the code in the future.

--- src/main/java/webUI/Pages.java
+++ src/main/java/webUI/Pages.java
@@ -11,8 +11,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Pages {
+    // load method takes a Javalin server and a Config object as arguments
     public static void load(Javalin server, Config config) {
+        // define a route for the root path and return the index HTML page
         server.get("/", ctx -> ctx.html(PageData.indexHtml()));
+        // define a route for the login path and redirect to the Discord OAuth URL
         server.get("/login", ctx -> {
             if (ctx.sessionAttribute("loggedIn") != "true") {
                 ctx.redirect(config.discordOAuthUrl());
@@ -20,21 +23,31 @@ public class Pages {
                 ctx.redirect("/");
             }
         });
+        // define a route for the receiveOAuth path and process the OAuth code and user data
         server.get("/receiveOAuth", ctx -> {
+            // get the OAuth code from the query parameter
             String code = ctx.queryParam("code");
+            // use the Config and code to get the access token
             String accessToken = OAuth2Handler.getAccessTokenFromCode(ConfigHandler.load(), code);
+            // use the access token to get the user JSON data
             String userJson = OAuth2Handler.getUserJsonByAccessToken(accessToken);
+            // print the user JSON data to the console (TODO: use userJson properly and remove this line)
             System.out.println(userJson);
+            // redirect the user back to the root path
             ctx.redirect("/");
         });
     }
 }
 
+// class for static methods that return page data
 class PageData {
+    // method that returns the index HTML page
     public static String indexHtml() throws IOException {
         String indexHtml;
 
+        // get the path to the index.html file
         Path path = Paths.get("Pages/index.html");
+        // read the contents of the file and convert to a string
         indexHtml = new String(Files.readAllBytes(path));
         return indexHtml;
     }
