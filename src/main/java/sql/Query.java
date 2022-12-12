9f5001e726cb225fa88c7379072e95bf9e2596c6
Notarin <424c414e4b@gmail.com>
2022-12-12 05:52:29 -0500

    Update saveUser method to check if user exists and update or insert accordingly
    This change updates the `saveUser` method in the `Query` class to first check if the user already exists in the database. If the user exists, the method constructs a `PreparedStatement` to update the user's information in the database. Otherwise, the method constructs a `PreparedStatement` to insert the user as a new record in the database.

--- src/main/java/sql/Query.java
+++ src/main/java/sql/Query.java
@@ -47,29 +47,58 @@ public class Query {
 
     // Save a User to the 'users' table in the database
     public static void saveUser(Connection connection, User user) throws SQLException {
-        // Create a prepared statement to insert the User data into the 'users' table
-        PreparedStatement statement = connection.prepareStatement("""
-            INSERT INTO users (
-                discordId,
-                username,
-                avatar,
-                discriminator,
-                banner,
-                locale,
-                email,
-                verified
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            """);
-        // Set the values of the prepared statement's parameters using the data from the User object
-        statement.setString(1, user.discordId());
-        statement.setString(2, user.username());
-        statement.setString(3, user.avatar());
-        statement.setInt(4, user.discriminator());
-        statement.setString(5, user.banner());
-        statement.setString(6, user.locale());
-        statement.setString(7, user.email());
-        statement.setBoolean(8, user.verified());
-        // Execute the prepared statement
+        // Check if the user already exists in the database
+        User existingUser = userById(connection, user.discordId());
+        PreparedStatement statement;
+        if (existingUser != null) {
+            // Create a prepared statement to update the User data in the 'users' table
+            statement = connection.prepareStatement("""
+                    UPDATE users
+                    SET
+                        username = ?,
+                        avatar = ?,
+                        discriminator = ?,
+                        banner = ?,
+                        locale = ?,
+                        email = ?,
+                        verified = ?
+                    WHERE discordId = ?
+                    """);
+            // Set the values of the prepared statement's parameters using the data from the User object
+            statement.setString(1, user.username());
+            statement.setString(2, user.avatar());
+            statement.setInt(3, user.discriminator());
+            statement.setString(4, user.banner());
+            statement.setString(5, user.locale());
+            statement.setString(6, user.email());
+            statement.setBoolean(7, user.verified());
+            statement.setString(8, user.discordId());
+            // Execute the prepared statement
+        } else {
+            // Create a prepared statement to insert the User data into the 'users' table
+            statement = connection.prepareStatement("""
+                    INSERT INTO users (
+                        discordId,
+                        username,
+                        avatar,
+                        discriminator,
+                        banner,
+                        locale,
+                        email,
+                        verified
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                    """);
+            // Set the values of the prepared statement's parameters using the data from the User object
+            statement.setString(1, user.discordId());
+            statement.setString(2, user.username());
+            statement.setString(3, user.avatar());
+            statement.setInt(4, user.discriminator());
+            statement.setString(5, user.banner());
+            statement.setString(6, user.locale());
+            statement.setString(7, user.email());
+            statement.setBoolean(8, user.verified());
+            // Execute the prepared statement
+        }
         statement.execute();
     }
 }
